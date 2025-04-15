2b4a4e3a32d74cc8a8ae1da6509109529b928779
gianbdev <giancarlo.silva550@gmail.com>
2025-04-15 12:13:10 -0500

    update | project section beta

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -2,13 +2,20 @@
 
 export default function projects() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
+     <section className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6 mx-auto max-w-6xl">
-        <div className="space-y-6 text-center">
-          <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl"> Next version
-          </h2>
+        {/* Aquí embebes la página de mantenimiento */}
+        <div className="w-full h-[500px] border rounded-xl overflow-hidden shadow-lg">
+          <iframe
+            src="https://maintenance-page-test.vercel.app/"
+            title="Under Maintenance"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            className="w-full h-full"
+          ></iframe>
         </div>
-        </div>
-      </section>
+      </div>
+    </section> 
   );
 }
